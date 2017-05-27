075ad6ddaaba4bbbe57e7bf353592d6468f6382a
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-05-27 22:43:14 +0530

    home left

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
 
     <!-- DEFAULT META TAGS -->
-    <?php
-    include('includes/head.php');
-    ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="files/css/mycss/bootstrap-grid.css">
     
     <!-- FONTS -->
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
